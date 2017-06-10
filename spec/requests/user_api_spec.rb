744e88dd46d567654d3fa7051a09374b3ce68cbd
Lee Saville <l.w.saville@gmail.com>
2017-06-10 16:49:05 -0600

    Change test setup, change url helpers to hit the api

--- spec/requests/user_api_spec.rb
+++ spec/requests/user_api_spec.rb
@@ -1,44 +1,81 @@
 require 'rails_helper'
 
-describe 'Retrieve user list from API', type: :request do
-  context 'with token authentication via query params' do
-    it 'returns status code 200' do
-      user = create(:user, :admin)
+describe 'User API endpoints' do
+  before(:each) do
+    @user = create(:user, :admin)
+  end
 
-      get admin_users_url(user_email: user.email,
-                          user_token: user.authentication_token), as: :json
+  describe 'Retrieve user list from API', type: :request do
+    context 'with token authentication via query params' do
+      it 'returns status code 200' do
+        get api_v1_users_url(user_email: @user.email,
+                            user_token: @user.authentication_token), as: :json
 
-      expect(response.status).to eq 200
+        expect(response.status).to eq 200
+      end
+
+      it 'returns valid user JSON matching schema' do
+        get api_v1_users_url(user_email: @user.email,
+                            user_token: @user.authentication_token), as: :json
+
+        expect(response).to match_response_schema('user')
+      end
     end
 
-    it 'returns valid user JSON matching schema' do
-      user = create(:user, :admin)
+    context 'with token authentication via request headers' do
+      it 'returns status code 200' do
+        token_header_params = { 'X-User-Email': @user.email,
+                                'X-User-Token': @user.authentication_token }
 
-      get admin_users_url(user_email: user.email,
-                          user_token: user.authentication_token), as: :json
+        get api_v1_users_url, headers: token_header_params, as: :json
 
-      expect(response).to match_response_schema('user')
+        expect(response.status).to eq 200
+      end
+      it 'returns value user JSON matching schema' do
+        token_header_params = { 'X-User-Email': @user.email,
+                                'X-User-Token': @user.authentication_token }
+
+        get api_v1_users_url, headers: token_header_params, as: :json
+
+        expect(response).to match_response_schema('user')
+      end
     end
   end
 
-  context 'with token authentication via request headers' do
-    it 'returns status code 200' do
-      user = create(:user, :admin)
-      token_header_params = { 'X-User-Email': user.email,
-                              'X-User-Token': user.authentication_token }
+  describe 'Retrieve a single user from API', type: :request do
+    context 'with token authentication via query params' do
+      it 'returns status code 200' do
+        get api_v1_user_url(user_email: @user.email,
+                            user_token: @user.authentication_token,
+                            id: @user.id), as: :json
 
-      get admin_users_url, headers: token_header_params, as: :json
-
-      expect(response.status).to eq 200
+        expect(response.status).to eq 200
+      end
+      it 'returns a single user JSON matching schema' do
+        get api_v1_user_url(user_email: @user.email,
+                            user_token: @user.authentication_token,
+                            id: @user.id), as: :json
+        expect(response).to match_response_schema('user')
+      end
     end
-    it 'returns value user JSON matching schema' do
-      user = create(:user, :admin)
-      token_header_params = { 'X-User-Email': user.email,
-                              'X-User-Token': user.authentication_token }
 
-      get admin_users_url, headers: token_header_params, as: :json
+    context 'with token authentication via request headers' do
+      it 'returns status code 200' do
+        token_header_params = { 'X-User-Email': @user.email,
+                                'X-User-Token': @user.authentication_token }
 
-      expect(response).to match_response_schema('user')
+        get api_v1_user_url(id: @user.id), headers: token_header_params, as: :json
+
+        expect(response.status).to eq 200
+      end
+      it 'returns a single user JSON matching schema' do
+        token_header_params = { 'X-User-Email': @user.email,
+                                'X-User-Token': @user.authentication_token }
+
+        get api_v1_user_url(id: @user.id), headers: token_header_params, as: :json
+
+        expect(response).to match_response_schema('user')
+      end
     end
   end
 end
