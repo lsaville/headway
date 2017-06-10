f86440545644782cfc6d10f9c9bbd0851ae28d13
Lee Saville <l.w.saville@gmail.com>
2017-06-10 17:28:31 -0600

    Add testing for the creation of a user via the API

--- spec/requests/user_api_spec.rb
+++ spec/requests/user_api_spec.rb
@@ -78,4 +78,58 @@ describe 'User API endpoints' do
       end
     end
   end
+
+  describe 'Create a user via the API', type: :request do
+    context 'with token authentication via query params' do
+      it 'returns status code 200' do
+        new_user_data = {email: 'oprah@example.com',
+                password: '12345678',
+                password_confirmation: '12345678'}
+
+        post api_v1_users_url(user_email: @user.email,
+                              user_token: @user.authentication_token,
+                              user: new_user_data), as: :json
+
+        expect(response.status).to eq 201
+      end
+      it 'returns the new user JSON matching schema' do
+        new_user_data = {email: 'oprah@example.com',
+          password: '12345678',
+          password_confirmation: '12345678'}
+
+          post api_v1_users_url(user_email: @user.email,
+          user_token: @user.authentication_token,
+          user: new_user_data), as: :json
+
+          expect(response).to match_response_schema('user')
+        end
+    end
+
+    context 'with token authentication via request headers' do
+      it 'returns status code 200' do
+        new_user_data = {email: 'oprah@example.com',
+                password: '12345678',
+                password_confirmation: '12345678'}
+
+        token_header_params = { 'X-User-Email': @user.email,
+                                'X-User-Token': @user.authentication_token }
+
+        post api_v1_users_url(user: new_user_data), headers: token_header_params, as: :json
+
+        expect(response.status).to eq 201
+      end
+      it 'returns the new user JSON matching schema' do
+        new_user_data = {email: 'oprah@example.com',
+                password: '12345678',
+                password_confirmation: '12345678'}
+
+        token_header_params = { 'X-User-Email': @user.email,
+                                'X-User-Token': @user.authentication_token }
+
+        post api_v1_users_url(user: new_user_data), headers: token_header_params, as: :json
+
+        expect(response).to match_response_schema('user')
+      end
+    end
+  end
 end
